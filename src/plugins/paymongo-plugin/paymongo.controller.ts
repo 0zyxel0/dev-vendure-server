96eb2709860b4d438468f5a1541d6472c755444d
Metono <calvin.ongkingco@gmail.com>
2024-04-24 12:22:47 +0200

    - updates on products

--- src/plugins/paymongo-plugin/paymongo.controller.ts
+++ src/plugins/paymongo-plugin/paymongo.controller.ts
@@ -20,8 +20,8 @@ export class PaymongoController{
         private connection: TransactionalConnection,
     ) {}
 
-    @Post('paymongo')
-    async webhook(@Req() req: RequestContext, @Res() res: Response) : Promise<void>{
+    // @Post('paymongo')
+    // async webhook(@Req() req: RequestContext, @Res() res: Response) : Promise<void>{
         
-    }
+    // }
 }
