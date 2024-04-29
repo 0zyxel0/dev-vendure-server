dab25ef75fde034ecbb17030ab59f619d2c9a8f1
Metono <calvin.ongkingco@gmail.com>
2024-04-29 02:41:21 +0200

    - updated vendure config - added order notes custom field - added order schedule collection time custom field

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -1,112 +1,140 @@
 import {
-    dummyPaymentHandler,
-    DefaultJobQueuePlugin,
-    DefaultSearchPlugin,
-    VendureConfig,
-} from '@vendure/core';
-import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
-import { AssetServerPlugin } from '@vendure/asset-server-plugin';
-import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
-import { MultivendorPlugin } from './plugins/multivendor-plugin/multivendor.plugin';
-import { paymongoPaymentHandler } from './plugins/paymongo-plugin/paymongo-handler';
-import 'dotenv/config';
-import path from 'path';
+  dummyPaymentHandler,
+  DefaultJobQueuePlugin,
+  DefaultSearchPlugin,
+  VendureConfig,
+  LanguageCode,
+} from '@vendure/core'
+import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin'
+import { AssetServerPlugin } from '@vendure/asset-server-plugin'
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin'
+import { MultivendorPlugin } from './plugins/multivendor-plugin/multivendor.plugin'
+import { paymongoPaymentHandler } from './plugins/paymongo-plugin/paymongo-handler'
+import 'dotenv/config'
+import path from 'path'
 
-const IS_DEV = process.env.APP_ENV === 'dev';
+const IS_DEV = process.env.APP_ENV === 'dev'
 
 export const config: VendureConfig = {
-    //@ts-ignore
-    cors:{
-        exposeHeaders: ['vendure-auth-token']
+  //@ts-ignore
+  cors: {
+    exposeHeaders: ['vendure-auth-token'],
+  },
+  apiOptions: {
+    port: process.env.APP_PORT,
+    adminApiPath: 'admin-api',
+    shopApiPath: 'shop-api',
+    // The following options are useful in development mode,
+    // but are best turned off for production for security
+    // reasons.
+    ...(IS_DEV
+      ? {
+          adminApiPlayground: {
+            settings: { 'request.credentials': 'include' },
+          },
+          adminApiDebug: true,
+          shopApiPlayground: {
+            settings: { 'request.credentials': 'include' },
+          },
+          shopApiDebug: true,
+        }
+      : {}),
+  },
+  authOptions: {
+    tokenMethod: ['bearer', 'cookie'],
+    superadminCredentials: {
+      identifier: process.env.SUPERADMIN_USERNAME,
+      password: process.env.SUPERADMIN_PASSWORD,
     },
-    apiOptions: {
-        port: process.env.APP_PORT,
-        adminApiPath: 'admin-api',
-        shopApiPath: 'shop-api',
-        // The following options are useful in development mode,
-        // but are best turned off for production for security
-        // reasons.
-        ...(IS_DEV ? {
-            adminApiPlayground: {
-                settings: { 'request.credentials': 'include' },
-            },
-            adminApiDebug: true,
-            shopApiPlayground: {
-                settings: { 'request.credentials': 'include' },
-            },
-            shopApiDebug: true,
-        } : {}),
+    cookieOptions: {
+      secret: process.env.COOKIE_SECRET,
     },
-    authOptions: {
-        tokenMethod: ['bearer', 'cookie'],
-        superadminCredentials: {
-            identifier: process.env.SUPERADMIN_USERNAME,
-            password: process.env.SUPERADMIN_PASSWORD,
-        },
-        cookieOptions: {
-          secret: process.env.COOKIE_SECRET,
-        },
-        // This Option will disable the email verification since all users are already verified from our main application.
-        requireVerification: false
-    },
-    dbConnectionOptions: {
-        type: 'better-sqlite3',
-        // See the README.md "Migrations" section for an explanation of
-        // the `synchronize` and `migrations` options.
-        // Run the synchoronize only once after that disable as the migrations will restart to the fresh database.
-        synchronize: false,
-        migrations: [path.join(__dirname, './migrations/*.+(js|ts)')],
-        logging: true,
-        database: path.join(__dirname, '../vendure.sqlite'),
-    },
-    paymentOptions: {
-        paymentMethodHandlers: [dummyPaymentHandler, paymongoPaymentHandler],
-    },
-    // When adding or altering custom field definitions, the database will
-    // need to be updated. See the "Migrations" section in README.md.
-    customFields: {},
-    plugins: [
-        AssetServerPlugin.init({
-            route: 'assets',
-            assetUploadDir: path.join(__dirname, '../static/assets'),
-            // For local dev, the correct value for assetUrlPrefix should
-            // be guessed correctly, but for production it will usually need
-            // to be set manually to match your production url.
-            assetUrlPrefix: IS_DEV ? undefined : 'http://localhost:3000/assets/',
-        }),
-        DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
-        DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
-        EmailPlugin.init({
-            devMode: true,
-            outputPath: path.join(__dirname, '../static/email/test-emails'),
-            route: 'mailbox',
-            handlers: defaultEmailHandlers,
-            templatePath: path.join(__dirname, '../static/email/templates'),
-            globalTemplateVars: {
-                // The following variables will change depending on your storefront implementation.
-                // Here we are assuming a storefront running at http://localhost:8080.
-                fromAddress: '"example" <noreply@example.com>',
-                verifyEmailAddressUrl: 'http://localhost:8080/verify',
-                passwordResetUrl: 'http://localhost:8080/password-reset',
-                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
-            },
-        }),
-        MultivendorPlugin.init({
-            platformFeePercent: 10,
-            platformFeeSKU: 'FEE',
-        }),
-        AdminUiPlugin.init({
-            route: 'admin',
-            port: 3000,
-            //Change the api host based on the dns being used
-            adminUiConfig: {
-              apiHost: process.env.FULL_VENDURE_HOST || 'http://localhost:3000',
-              // Comment out the apiPort when you do not use the custom dns or localhost
-            //   apiPort: 443,
-              adminApiPath: 'admin-api',
-              hideVendureBranding: true,
-              hideVersion: true,
-            },
-        }),
+    // This Option will disable the email verification since all users are already verified from our main application.
+    requireVerification: false,
+  },
+  dbConnectionOptions: {
+    type: 'better-sqlite3',
+    // See the README.md "Migrations" section for an explanation of
+    // the `synchronize` and `migrations` options.
+    // Run the synchoronize only once after that disable as the migrations will restart to the fresh database.
+    synchronize: false,
+    migrations: [path.join(__dirname, './migrations/*.+(js|ts)')],
+    logging: true,
+    database: path.join(__dirname, '../vendure.sqlite'),
+  },
+  paymentOptions: {
+    paymentMethodHandlers: [dummyPaymentHandler, paymongoPaymentHandler],
+  },
+  // When adding or altering custom field definitions, the database will
+  // need to be updated. See the "Migrations" section in README.md.
+  customFields: {
+    //Apply Order Notes as an optional field for customers
+    Order: [
+      {
+        name: 'orderNotes',
+        type: 'string',
+        label: [
+          {
+            languageCode: LanguageCode.en,
+            value: 'Order Notes',
+          },
+        ],
+      },
+      {
+        name: 'orderScheduleCollection',
+        type: 'datetime',
+        label: [
+          {
+            languageCode: LanguageCode.en,
+            value: 'Order Schedule',
+          },
+        ],
+      }
     ],
-};
+  },
+  plugins: [
+    AssetServerPlugin.init({
+      route: 'assets',
+      assetUploadDir: path.join(__dirname, '../static/assets'),
+      // For local dev, the correct value for assetUrlPrefix should
+      // be guessed correctly, but for production it will usually need
+      // to be set manually to match your production url.
+      assetUrlPrefix: IS_DEV ? undefined : 'http://localhost:3000/assets/',
+    }),
+    DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
+    DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
+    EmailPlugin.init({
+      devMode: true,
+      outputPath: path.join(__dirname, '../static/email/test-emails'),
+      route: 'mailbox',
+      handlers: defaultEmailHandlers,
+      templatePath: path.join(__dirname, '../static/email/templates'),
+      globalTemplateVars: {
+        // The following variables will change depending on your storefront implementation.
+        // Here we are assuming a storefront running at http://localhost:8080.
+        fromAddress: '"example" <noreply@example.com>',
+        verifyEmailAddressUrl: 'http://localhost:8080/verify',
+        passwordResetUrl: 'http://localhost:8080/password-reset',
+        changeEmailAddressUrl:
+          'http://localhost:8080/verify-email-address-change',
+      },
+    }),
+    MultivendorPlugin.init({
+      platformFeePercent: 10,
+      platformFeeSKU: 'FEE',
+    }),
+    AdminUiPlugin.init({
+      route: 'admin',
+      port: 3000,
+      //Change the api host based on the dns being used
+      adminUiConfig: {
+        apiHost: process.env.FULL_VENDURE_HOST || 'http://localhost:3000',
+        // Comment out the apiPort when you do not use the custom dns or localhost
+        //   apiPort: 443,
+        adminApiPath: 'admin-api',
+        hideVendureBranding: true,
+        hideVersion: true,
+      },
+    }),
+  ],
+}
